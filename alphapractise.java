0b1b57329681851df30b90e911628fba52e05d23
sudhanshu576 <sudhanshuroy576@gmail.com>
2023-10-20 19:18:02 +0530

    sunny

--- alphapractise.java
+++ alphapractise.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class alphapractise {
     public static void main(String[] args) {
         //question 1
-       /* Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter 1st nymber = ");
         int a = sc.nextInt();
         System.out.print("Enter 2nd number = ");
@@ -10,7 +10,7 @@ public class alphapractise {
         System.out.print("Enter 3rd number = ");
         int c = sc.nextInt();
         System.out.println("Average = " +(a+b+c)/3);
-        */
+        
 
         //question 2
         
